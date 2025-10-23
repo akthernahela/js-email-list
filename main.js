608e1f08a6b60ea4d106b75212ec1c54cc6b08ce
akthernahela <nahela00@gmail.com>
2025-10-23 15:27:02 +0200

    Add the description of today's exercise

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+Esercizio di oggi: Email List
+
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+*/
