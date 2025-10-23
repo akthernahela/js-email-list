19cfde45309d9a40ed08537c48ecbe91fcc7f176
akthernahela <nahela00@gmail.com>
2025-10-23 15:29:42 +0200

    Add the postman verification result

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 */
+
+//Postman verification result of the email:
+/*
+- https://flynn.boolean.careers/exercises/api/random/mail
+{
+    "success": true,
+    "response": "ngraham@fadel.biz"
+}
+*/
